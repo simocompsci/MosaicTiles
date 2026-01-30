47f62e10b7f43cd78abb6f31ac19b55a6f12ea02
simocompsci <hamids9alo7777@gmail.com>
2026-01-30 22:14:15 +0100

    some changes in the navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -16,17 +16,15 @@ export default function Navbar() {
                 {/* Center: Logo */}
                 <div className="flex-1 flex justify-start">
                     <Link href="/" className="inline-block relative">
-                        <h1 className="text-5xl md:text-6xl text-[#6B8E7B] font-normal leading-none tracking-tighter" style={{ fontFamily: 'var(--font-libre-bodoni)' }}>
-                            LOGO
+                        <h1 className="text-4xl md:text-6xl text-[#6B8E7B] font-normal leading-none tracking-tighter" style={{ fontFamily: 'var(--font-libre-bodoni)' }}>
+                            MoroccanMosaic
                         </h1>
-                        {/* Overlay the "O" to create the monogram effect if possible, or just use text for now */}
-                        {/* For now, just "THE" text centered as per screenshot structure */}
                     </Link>
                 </div>
 
                 {/* center div */}
                 <div>
-                    <ul className='flex justify-between items-center gap-15 text-lg'>
+                    <ul className='flex justify-between items-center gap-20 text-lg'>
                         <li>About Us</li>
                         <li>Collections</li>
                         <li>Gallery</li>
@@ -36,11 +34,10 @@ export default function Navbar() {
 
                 {/* Right: Account & Cart */}
                 <div className="flex-1 flex justify-end items-center gap-6 md:gap-8">
-                    <Link href="/account" className="text-xs md:text-xs text-gray-500 hover:text-gray-800 uppercase tracking-wider font-light transition-colors">
-                        Account
-                    </Link>
-                    <Link href="/cart" className="text-xs md:text-xs text-gray-500 hover:text-gray-800 uppercase tracking-wider font-light transition-colors">
-                        Cart (0)
+                    <Link href="/contact">
+                        <button className='bg-black text-white text-sm md:text-lg tracking-wider font-light p-5 hover:bg-white hover:text-black hover:border-2 hover:border-black transition-all duration-100 ease-in-out hover:cursor-pointer'>
+                            Contact Us
+                        </button>
                     </Link>
                 </div>
             </div>
